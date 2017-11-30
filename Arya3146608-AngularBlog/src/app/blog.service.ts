9c3c9037a728435b32b33d7cffaea0aa97e9161e
aaryasharma <30898257+aaryasharma@users.noreply.github.com>
2017-11-30 15:27:02 +0530

    Update blog.service.ts

--- Arya3146608-AngularBlog/src/app/blog.service.ts
+++ Arya3146608-AngularBlog/src/app/blog.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import{Http,Headers} from "@angular/http";
 import 'rxjs/add/operator/map';
 
-const BASE_URL = 'http://localhost:3000/blogs/';
+const BASE_URL = 'https://angular-database.herokuapp.com/blogs';
 const header = {headers: new Headers({'Content-Type': 'application/json'})}
 
 @Injectable()
@@ -24,17 +24,17 @@ export class BlogService {
   }
 
   deleteData(id:number){
-    return this.http.delete(BASE_URL+id)
+    return this.http.delete(BASE_URL+"/"+id)
       .map(res=> res.json())
   }
 
   putData(data){
-    return this.http.put(BASE_URL+data.id,data)
+    return this.http.put(BASE_URL+"/"+data.id,data)
       .map(res=>res.json())
   }
 
   getDatabyid(id:number){
-    return this.http.get(BASE_URL+id)
+    return this.http.get(BASE_URL+"/"+id)
       .map(res => res.json());
   }
 
